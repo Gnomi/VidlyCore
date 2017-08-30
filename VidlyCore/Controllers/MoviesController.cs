6c3c0531c0baf910a00c1dc3d8b2fd27112afad7
Jon <jonathan.c.leung@gmail.com>
2017-08-30 15:13:45 -0500

    Add project files.

---
+++ VidlyCore/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VidlyCore.Models;
+
+namespace VidlyCore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movies = GetMovies();
+
+
+            return View(movies);
+        }
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+             return new List<Movie>
+             {
+                 new Movie { Id = 1, Name = "Shrek" },
+                 new Movie { Id = 2, Name = "Wall-e" }
+             };
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(^\\d{{2}}$)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+
+            var movie = new Movie { Name = "Sherk Reunion!" };
+
+
+            return View(movie);
+        }
+    }
+}
